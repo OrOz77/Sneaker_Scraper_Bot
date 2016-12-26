a62adb43d71080d24cf03960493949e38b70a84f
Or Oz <oroz77@gmail.com>
2016-12-25 21:19:42 -0500

    added Pipeline for MongoDB storage

--- Nike/Nike/pipelines.py
+++ Nike/Nike/pipelines.py
@@ -4,8 +4,37 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
+from Nike.MongoDB.config import MONGODB_URI, MONGODB_DB
 
 
 class NikePipeline(object):
     def process_item(self, item, spider):
+        return item
+
+#Pipeline for storing to MongoDB
+class MongoPipeline(object):
+
+    collection_name = 'Nike.com Shoes'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=MONGODB_URI,
+            mongo_db=MONGODB_DB
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        self.db[self.collection_name].insert(dict(item)) #store item to collection
         return item
