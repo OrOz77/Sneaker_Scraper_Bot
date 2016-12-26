0721fe8366a59c23668fd9d56efed880fcca82d3
Or Oz <oroz77@gmail.com>
2016-12-25 21:17:32 -0500

    folder for MongoDB settings

---
+++ Nike/Nike/MongoDB/sample_config.py
@@ -0,0 +1,9 @@
+#CREATE A FILE 'config.py' IN THIS DIRECTORY
+#'config.py' is in .gitignore
+#COPY AND PASTE THIS FILE, EDIT RELEVANT FIELDS
+
+MONGODB_URI = 'mongodb://<user>:<password>@<address>/<db_name>' #can be found on MongoDB Dashboard
+MONGODB_DB = '<db_name>'
+
+MONGODB_SERVER = "localhost"    #default value
+MONGODB_PORT = 27017            #default value
