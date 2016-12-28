e3caee666f7e03da25c68b16012e9933f6cc6d83
Or Oz <oroz77@gmail.com>
2016-12-28 16:47:49 -0500

    send tweet via tweepy working

--- Bot/Sneaker_Scraper/Twitter/hello.py
+++ Bot/Sneaker_Scraper/Twitter/hello.py
@@ -1,14 +1,15 @@
 import tweepy
-from config import TWITTER_CONSUMER_TOKEN, TWITTER_CONSUMER_SECRET
+from config import TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET
 
-#tut from http://tweepy.readthedocs.io/en/v3.5.0/auth_tutorial.html#auth-tutorial
+#tut from http://pythoncentral.io/introduction-to-tweepy-twitter-for-python/
 
-auth = tweepy.OAuthHandler(TWITTER_CONSUMER_TOKEN, TWITTER_CONSUMER_SECRET)
-
+auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
 try:
     redirect_url = auth.get_authorization_url()
 except tweepy.TweepError:
     print 'Error! Failed to get request token.'
 
 api = tweepy.API(auth)
+
 api.update_status('tweepy + oauth!')
