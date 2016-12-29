d7013aa5af695dbcfb047944357d8b3b75abfde4
Or Oz <oroz77@gmail.com>
2016-12-29 16:54:31 -0500

    added upsert for mongodb storage to prevent duplicates and add new entries

--- Bot/Sneaker_Scraper/pipelines.py
+++ Bot/Sneaker_Scraper/pipelines.py
@@ -34,5 +34,14 @@ class MongoPipeline(object):
         self.client.close()
 
     def process_item(self, item, spider):
-        self.db[item['urlSource']].insert(dict(item)) #store item to collection by source
+        collection = self.db[item['urlSource']]   #url source is collection name (ie Nike.com, Adidas.com, etc)
+        collection.update_one(
+            {
+                'modelNumber': item['modelNumber']      #index collection by modelNumber
+            },
+            {
+                '$set': dict(item)                      #update item. TODO look into only updating size field, but allows upsert to add entire item
+            },
+                upsert=True)                            #upsert allows new entry if update not found
+
         return item
